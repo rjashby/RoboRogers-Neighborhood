1ac652c8e15d2eb9bd05e5ce02736d480ff69a45
Ryan Ashby <rjapsu160@gmail.com>
2021-10-29 09:10:55 -0700

    add initial UI logic in scripts.

---
+++ README.md
@@ -0,0 +1,6 @@
+## Spec Tests
+
+### Description: roboRogers()
+*Test:* "It will add 'way' to the end of words that begin with a vowel."
+*Code:* pigLatin("a");
+*Expected output:* "away"
---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// UI Logic //
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault()
+    $("#results").hide();
+    const yourNumber = parseInt($("input#formNumber").val());
+    if ($("#formNumber").val() == "") {
+      return ("")
+      alert("Please Enter A Number");
+    } else ($("#formNumber").val() ==  {
+      return (number)
+    }
+  });
+
+
+// Business Logic //
+function roboRogers();
