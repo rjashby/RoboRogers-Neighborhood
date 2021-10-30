26864d14671b697d6130e5a80593ad387d6d8381
Ryan Ashby <rjapsu160@gmail.com>
2021-10-30 13:16:54 -0700

    YES! combined countUp function with overall function to split double digit nums.

--- js/scriptplayground.js
+++ js/scriptplayground.js
@@ -7,16 +7,8 @@ function countUp(number) {
  console.log(numberArray);
 };
 
-
-// } else if (responsiveNumbers[0 && 2].includes(number)) {
-//  rogersResponse.push(responseList[0]);
-//} else if (responsiveNumbers[1 && 0].includes(number)) {
-//  rogersResponse.push(responseList[1]);
-//} else if (responsiveNumbers[2 && 1].includes(number)) {
-//  rogersResponse.push(responseList[2]);
-
 function roboRogers(yourNumber) {
-  console.log(yourNumber);
+  console.log(roboRogers);
   const responseList = ["Beep!", "Boop!", "Won't you be my neighbor?"]
   const responsiveNumbers = ["1", "2", "3"];
   let splitNumbers = yourNumber.toString().split("");
@@ -35,6 +27,49 @@ function roboRogers(yourNumber) {
     return responseList[2];
   };
   if (yourNumber === [4, 5 ,6, 7, 8, 9]) {
+    return yourNumber
+  };
+  splitNumbers.forEach(function(number) {
+    if (responsiveNumbers[0].includes(number)) {
+      rogersResponse.push(responseList[0]);
+    } else if (responsiveNumbers[1].includes(number)) {
+      rogersResponse.push(responseList[1]);
+    } else if (responsiveNumbers[2].includes(number)) {
+      rogersResponse.push(responseList[2]);
+    } else {
+      rogersResponse.push(number)
+    };
+  });
+  return rogersResponse.join(" ");
+};
+
+
+
+function roboRogers(yourNumber) {
+  let numberArray = [];
+    for (let index = 0; index <= yourNumber; index ++) {
+      numberArray.push(index)
+    };
+    numberArray;
+    console.log(numberArray);
+  const responseList = ["Beep!", "Boop!", "Won't you be my neighbor?"]
+  const responsiveNumbers = ["1", "2", "3"];
+  let splitNumbers = numberArray.toString().split("");
+  let rogersResponse = [];
+  console.log(splitNumbers)
+  if (splitNumbers === 0) {
+    return 0;
+  };
+  if (splitNumbers == 1) {
+    return responseList[0];
+  };
+  if (splitNumbers == 2) {
+    return responseList[1];
+  };
+  if (splitNumbers == 3) {
+    return responseList[2];
+  };
+  if (splitNumbers === [4, 5 ,6, 7, 8, 9]) {
     return yourNumber
   };
   splitNumbers.forEach(function(number) {
--- js/scripts.js
+++ js/scripts.js
@@ -10,41 +10,46 @@ $(document).ready(function() {
         alert("Please Enter A Number");
       } else {
         $("#results").fadeIn();
-      };
-    });
-  });
-    function roboRogers(yourNumber) {
-      console.log(yourNumber);
-      const responseList = ["Beep!", "Boop!", "Won't you be my neighbor?"]
-      const responsiveNumbers = ["1", "2", "3"];
-      let splitNumbers = yourNumber.toString().split("");
-      let rogersResponse = [];
-      console.log(splitNumbers)
-      if (yourNumber === 0) {
-        return 0;
-      };
-      if (yourNumber == 1) {
-        return responseList[0];
-      };
-      if (yourNumber == 2) {
-        return responseList[1];
-      };
-      if (yourNumber == 3) {
-        return responseList[2];
-      };
-      if (yourNumber === [4, 5 ,6, 7, 8, 9]) {
-        return yourNumber
-      };
-      splitNumbers.forEach(function(number) {
-        if (responsiveNumbers[0].includes(number)) {
-          rogersResponse.push(responseList[0]);
-        } else if (responsiveNumbers[1].includes(number)) {
-          rogersResponse.push(responseList[1]);
-        } else if (responsiveNumbers[2].includes(number)) {
-          rogersResponse.push(responseList[2]);
-        } else {
-          rogersResponse.push(number)
+      }
+      function roboRogers(yourNumber) {
+        let numberArray = [];
+          for (let index = 0; index <= yourNumber; index ++) {
+            numberArray.push(index)
+          };
+          numberArray;
+          console.log(numberArray);
+        const responseList = ["Beep!", "Boop!", "Won't you be my neighbor?"]
+        const responsiveNumbers = ["1", "2", "3"];
+        let splitNumbers = numberArray.toString().split("");
+        let rogersResponse = [];
+        console.log(splitNumbers)
+        if (splitNumbers === 0) {
+          return 0;
         };
-      });
-      return rogersResponse.join(" ");
-    };
+        if (splitNumbers == 1) {
+          return responseList[0];
+        };
+        if (splitNumbers == 2) {
+          return responseList[1];
+        };
+        if (splitNumbers == 3) {
+          return responseList[2];
+        };
+        if (splitNumbers === [4, 5 ,6, 7, 8, 9]) {
+          return yourNumber
+        };
+        splitNumbers.forEach(function(number) {
+          if (responsiveNumbers[0].includes(number)) {
+            rogersResponse.push(responseList[0]);
+          } else if (responsiveNumbers[1].includes(number)) {
+            rogersResponse.push(responseList[1]);
+          } else if (responsiveNumbers[2].includes(number)) {
+            rogersResponse.push(responseList[2]);
+          } else {
+            rogersResponse.push(number)
+          };
+        });
+        return rogersResponse.join(" ");
+      };
+  });
+});
