66ea57a0c5d50f8aeb5a4ab1fc86e28dd095f0f4
Ryan Ashby <rjapsu160@gmail.com>
2021-11-01 14:52:42 -0700

    add parseInt to initial value to be captured.

--- js/scriptplayground.js
+++ js/scriptplayground.js
@@ -46,6 +46,7 @@ function roboRogers(yourNumber) {
 
 
 function roboRogers(yourNumber) {
+  console.log(yourNumber);
   let numberArray = [];
     for (let index = 0; index <= yourNumber; index ++) {
       numberArray.push(index)
@@ -90,3 +91,50 @@ if (splitNumbers == 3) {
 if (splitNumbers === [4, 5 ,6, 7, 8, 9]) {
   return yourNumber
 };
+
+if (splitNumbers === (1) && (3)) {
+  return 2;
+}
+
+
+// all together //
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault()
+    $("#results").hide();
+    const yourNumber = $("input#formNumber").val();
+    console.log(yourNumber); 
+    function roboRogers(yourNumber) {
+      let numberArray = [];
+        for (let index = 0; index <= yourNumber; index ++) {
+          numberArray.push(index)
+        };
+        numberArray;
+        console.log(numberArray);
+      const responseList = ["Beep!", "Boop!", "Won't you be my neighbor?"]
+      const responsiveNumbers = ["1", "2", "3"];
+      let splitNumbers = numberArray.toString().split("");
+      let rogersResponse = [];
+      console.log(splitNumbers)
+      splitNumbers.forEach(function(number) {
+        if (responsiveNumbers[0].includes(number)) {
+          rogersResponse.push(responseList[0]);
+        } else if (responsiveNumbers[1].includes(number)) {
+          rogersResponse.push(responseList[1]);
+        } else if (responsiveNumbers[2].includes(number)) {
+          rogersResponse.push(responseList[2]);
+        } else {
+          rogersResponse.push(number)
+        };
+      });
+      return rogersResponse.join(" ");
+      };
+      if ($("#formNumber").val() == "") {
+        $("#results").hide();
+        alert("Please Enter A Number");
+      } else {
+        $("#results").fadeIn();
+        $("#rogersResponse").html(rogersResponse);
+      }
+  });
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,41 @@
-// UI Logic //
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault()
     $("#results").hide();
-    const yourNumber = $("input#formNumber").val();
-    console.log(yourNumber);
+    let yourNumber = parseInt($("input#formNumber").val());
+    console.log(yourNumber); 
+    function roboRogers(yourNumber) {
+      console.log(yourNumber);
+      let numberArray = [];
+        for (let index = 0; index <= yourNumber; index ++) {
+          numberArray.push(index)
+        };
+        numberArray;
+        console.log(numberArray);
+      const responseList = ["Beep!", "Boop!", "Won't you be my neighbor?"]
+      const responsiveNumbers = ["1", "2", "3"];
+      let splitNumbers = numberArray.toString().split("");
+      let rogersResponse = [];
+      console.log(splitNumbers)
+      splitNumbers.forEach(function(number) {
+        if (responsiveNumbers[0].includes(number)) {
+          rogersResponse.push(responseList[0]);
+        } else if (responsiveNumbers[1].includes(number)) {
+          rogersResponse.push(responseList[1]);
+        } else if (responsiveNumbers[2].includes(number)) {
+          rogersResponse.push(responseList[2]);
+        } else {
+          rogersResponse.push(number)
+          $("#final").text(rogersResponse);
+        };
+      });
+      return rogersResponse.join(" ");
+      };
       if ($("#formNumber").val() == "") {
         $("#results").hide();
         alert("Please Enter A Number");
       } else {
         $("#results").fadeIn();
       }
-      function roboRogers(yourNumber) {
-        let numberArray = [];
-          for (let index = 0; index <= yourNumber; index ++) {
-            numberArray.push(index)
-          };
-          numberArray;
-          console.log(numberArray);
-        const responseList = ["Beep!", "Boop!", "Won't you be my neighbor?"]
-        const responsiveNumbers = ["1", "2", "3"];
-        let splitNumbers = numberArray.toString().split("");
-        let rogersResponse = [];
-        console.log(splitNumbers)
-        splitNumbers.forEach(function(number) {
-          if (responsiveNumbers[0].includes(number)) {
-            rogersResponse.push(responseList[0]);
-          } else if (responsiveNumbers[1].includes(number)) {
-            rogersResponse.push(responseList[1]);
-          } else if (responsiveNumbers[2].includes(number)) {
-            rogersResponse.push(responseList[2]);
-          } else {
-            rogersResponse.push(number)
-          };
-        });
-        return rogersResponse.join(" ");
-      };
   });
 });
